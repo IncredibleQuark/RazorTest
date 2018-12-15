a8f316a040c1ad3b223668389f496917bde33f35
lukasz kupinski <lkupinski@gmail.com>
2018-12-15 21:08:06 +0100

    Create CRUD for Movie class, execute migration

--- RazorTest/Startup.cs
+++ RazorTest/Startup.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using RazorTest.Models;
 
 namespace RazorTest
 {
@@ -33,6 +35,9 @@ namespace RazorTest
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<RazorTestContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("RazorTestContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
